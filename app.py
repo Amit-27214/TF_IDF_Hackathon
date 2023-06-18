ffff27d82b8507e9d5ddb2abccaa6a6b845eb2eb
Amit-27214 <84027408+Amit-27214@users.noreply.github.com>
2023-06-18 15:22:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from wtforms import StringField, SubmitField
 
 def load_vocab():
     vocab = {}
-    with open('vocab.txt', 'r') as f:
+    with open('vocab.txt', 'r', encoding = 'latin-1') as f:
         vocab_terms = f.readlines()
-    with open('idf-values.txt', 'r') as f:
+    with open('idf-values.txt', 'r', encoding = 'latin-1') as f:
         idf_values = f.readlines()
     
     for (term, idf_value) in zip(vocab_terms, idf_values):
@@ -18,7 +18,7 @@ def load_vocab():
 
 def load_documents():
     documents = []
-    with open('documents.txt', 'r') as f:
+    with open('documents.txt', 'r', encoding = 'latin-1') as f:
         documents = f.readlines()
     documents = [document.strip().split() for document in documents]
 
@@ -28,7 +28,7 @@ def load_documents():
 
 def load_inverted_index():
     inverted_index = {}
-    with open('inverted-index.txt', 'r') as f:
+    with open('inverted-index.txt', 'r', encoding = 'latin-1') as f:
         inverted_index_terms = f.readlines()
 
     for row_num in range(0, len(inverted_index_terms), 2):
@@ -40,12 +40,10 @@ def load_inverted_index():
     return inverted_index
 
 def load_link_of_qs():
-    with open("Qindex.txt", 'r') as f:
+    with open("Qindex.txt", 'r', encoding = 'latin-1') as f:
         links = f.readlines()
 
     return links
-
-# The rest of the code remains the same...
 
 
 def find_index(l1, l2):
@@ -92,7 +90,7 @@ def get_idf_value(term):
 filename = 'index.txt'
 
 # Read the content of the file
-with open(filename, 'r') as f:
+with open(filename, 'r', encoding = 'latin-1') as f:
     lines = f.readlines()
 
 # Convert all lines to lowercase
